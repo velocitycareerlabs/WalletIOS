102c2ec3af0218efd4819664a7148e393f91ee08
Michael Avoyan <michael.avoyan.dev@gmail.com>
2022-11-30 22:25:05 +0200

    v1.1.9

---
+++ VCL/VCLTests/entities/VCLSubmissionResultTest.swift
@@ -0,0 +1,40 @@
+//
+//  VCLSubmissionResultTest.swift
+//  VCLTests
+//
+//  Created by Michael Avoyan on 30/11/2022.
+//
+//  Copyright 2022 Velocity Career Labs inc.
+//  SPDX-License-Identifier: Apache-2.0
+
+import Foundation
+import XCTest
+@testable import VCL
+
+class VCLSubmissionResultTest: XCTestCase {
+    var subject: VCLSubmissionResult!
+
+    override func setUp() {
+        subject = VCLSubmissionResult(
+            token: VCLToken(value: "token123"),
+            exchange: VCLExchange(
+                id: "id123",
+                type: "type123",
+                disclosureComplete: true,
+                exchangeComplete: true
+            ),
+            jti: "jti123",
+            submissionId: "submissionId123"
+        )
+    }
+
+    func testProps() {
+        assert(subject.token.value == "token123")
+        assert(subject.exchange.id == "id123")
+        assert(subject.exchange.type == "type123")
+        assert(subject.exchange.exchangeComplete == true)
+        assert(subject.exchange.disclosureComplete == true)
+        assert(subject.jti == "jti123")
+        assert(subject.submissionId == "submissionId123")
+    }
+}
