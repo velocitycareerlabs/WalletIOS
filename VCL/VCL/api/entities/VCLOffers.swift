742a0a9a37a5bee215fecb446b508f55785baf36
Michael Avoyan <michael.avoyan.dev@gmail.com>
2023-04-27 17:13:37 +0300

    v1.11.4 fix offers

--- VCL/VCL/api/entities/VCLOffers.swift
+++ VCL/VCL/api/entities/VCLOffers.swift
@@ -4,34 +4,19 @@
 //
 //  Created by Michael Avoyan on 11/05/2021.
 //
-// Copyright 2022 Velocity Career Labs inc.
-// SPDX-License-Identifier: Apache-2.0
+//  Copyright 2022 Velocity Career Labs inc.
+//  SPDX-License-Identifier: Apache-2.0
 
 import Foundation
 
 public struct VCLOffers {
-    public let payload: [String: Any]
     public let all: [[String: Any]]
     public let responseCode: Int
     public let token: VCLToken
-    public let challenge: String
     
-    public init(
-        payload: [String: Any],
-        all: [[String: Any]],
-        responseCode: Int,
-        token: VCLToken,
-        challenge: String
-    ) {
-        self.payload = payload
+    public init(all: [[String: Any]], responseCode: Int, token: VCLToken) {
         self.all = all
         self.responseCode = responseCode
         self.token = token
-        self.challenge = challenge
-    }
-    
-    public struct CodingKeys {
-        public static let KeyOffers = "offers"
-        public static let KeyChallenge = "challenge"
     }
 }
